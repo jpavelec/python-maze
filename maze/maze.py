caf00c401ab4c13424d4b29c2060e34766fd86f4
jpavelec <jospavelec@gmail.com>
2017-01-20 11:15:38 +0100

    Make maze from string

--- maze/maze.py
+++ maze/maze.py
@@ -13,16 +13,29 @@ class MazeGame(object):
         Pokud start nebo stop je mimo bludiste, vyvola vyjimku
         """
         assert data.dtype == bool
+        
+        self.data = data
+        
+        if (start[0]<0) or (start[0]>self.getSize()[0]) or (start[1]<0) or (start[1]>self.getSize()[1]):
+            raise MazeError("Start position is out of maze")
+        if (end[0]<0) or (end[0]>self.getSize()[0]) or (end[1]<0) or (end[1]>self.getSize()[1]):
+            raise MazeError("End position is out of maze")
+        
+        self.start = start
+        self.end = end
+        '''print(data)
+        print(start)
+        print(end)'''
 
     def getSize(self):
         "Vrati velikost bludiste"
         return self.data.shape
 
     def getStart(self):
-        return (0,0)
+        return self.start
 
     def getEnd(self):
-        return (10, 10)
+        return self.end
 
     def isFree(self, x, y):
         "Vrátí False jestli na pozici x,y chodba. Jinak vrací True"
@@ -33,14 +46,55 @@ class MazeGame(object):
         return MazePath(steps=[])
 
     @staticmethod 
-    def fromString(data):
+    def fromString(maze):
         """
-        Postavi bludiste z retezce. 
-        Prazdne radky se ignoruji, 
-        X nebo # je zed,  B je zacatek a E je konec.
+        Build maze from string.
+        Empty lines are ignored, 
+        X or # is wall,  B represents start and E represents goal of maze.
         """
-        data = numpy.array([[True, True, True], [False, True, True]])
-        return MazeGame(data, start=(0,0), end=(1,2))
+        
+        lines = maze.splitlines()
+        
+        # ... if line remove empty line
+        input_data = [list(line) for line in lines if line]
+        input_data = numpy.asarray(input_data)
+        
+        if (len(input_data.shape)<2):
+            raise MazeError("Maze is not regular! Some row(s) has different length than others.")
+        
+        HEIGHT = input_data.shape[0]
+        WIDTH = input_data.shape[1]
+
+        data = numpy.empty((HEIGHT,WIDTH),dtype=bool)
+
+        start_count, end_count = 0, 0
+
+        for i in range(HEIGHT):
+            for j in range(WIDTH):
+                if (input_data[i,j] == 'X') or (input_data[i,j] == '#'):
+                    data[i,j] = False
+                elif input_data[i,j] == 'B':
+                    if start_count>0:
+                        raise MazeError("Too many begins!")
+                    start_count += 1
+                    start = (i,j)
+                    data[i,j] = True
+                elif input_data[i,j] == 'E':
+                    if end_count > 0:
+                        raise MazeError("Too many ends!")
+                    end_count += 1
+                    end = (i,j)
+                    data[i,j] = True
+                else:
+                    data[i,j] = True
+                
+            
+        if start_count == 0:
+            raise MazeError("There is no begin!")
+        if end_count == 0:
+            raise MazeError("There is no end!")
+        
+        return MazeGame(data, start, end)
 
 class MazePath(object):
     "Objekt obsahujici cestu v bludisti"
